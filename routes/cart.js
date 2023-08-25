b06d0843e974fac98806c511b9372792526c5821
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-08-25 13:18:26 -0400

    update views with heading accent font, remove theme reference from public pages

--- routes/cart.js
+++ routes/cart.js
@@ -4,4 +4,6 @@ const cartController = require('../controllers/cart')
 
 router.get('/', cartController.getIndex) //uses home controller to get index page and render index.js from views
 
+router.put('/removeFromCart', cartController.removeFromCart)
+
 module.exports = router
--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,15 @@ router.get('/', shopController.getIndex) //uses home controller to get index pag
 
 // router.get(`/:id`, shopController.seePlant)
 
+router.get('/sortPriceLowHigh', shopController.sortPriceLowHigh)
+router.get('/sortPriceHighLow', shopController.sortPriceHighLow)
+router.get('/sortAZ', shopController.sortAZ)
+router.get('/sortZA', shopController.sortZA)
+
+router.get('/filterPothos', shopController.filterPothos)
+router.get('/filterPhilos', shopController.filterPhilos)
+router.get('/filterOthers', shopController.filterOthers)
+
 router.put('/addToCart', shopController.addToCart)
 
 module.exports = router
