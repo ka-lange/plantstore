468454d2d576cbeaa5b145a451277ac1a22d7cfc
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-08-25 13:19:04 -0400

    add photos and styling for index

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,10 @@
-
+const Plant = require('../models/Plant')
 module.exports = {
     getIndex: async (req,res)=>{
-        // res.render('index.ejs') //renders ejs file and reponds with it
+       
         try{
-            // const Habits = await Habit.find()//uses variable Todo model to grab todo instances from database, mongoose automatically turns to array
-            // const itemsLeft = await Todo.countDocuments({completed: false})
-            res.render('index.ejs')
+            const Plants = await Plant.find()
+            res.render('index.ejs', {plants: Plants})
         }catch(err){
             console.log(err)
         }
